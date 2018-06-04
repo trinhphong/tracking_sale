78c888a3c82cb06dafb1ff47b9c354159632ebad
TrinhPhong <nguyenquoctin921996@gmail.com>
2018-06-04 22:17:53 +0700

    Add default value

--- db/migrate/20180526084223_add_actual_quantity_to_tasks.rb
+++ db/migrate/20180526084223_add_actual_quantity_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddActualQuantityToTasks < ActiveRecord::Migration[5.0]
   def change
-    add_column :tasks, :actual_quantity, :integer
+    add_column :tasks, :actual_quantity, :integer, default: 0
   end
 end
